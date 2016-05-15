808e1f516a91056e97fd33c9bbf07a3d03fdab86
Lana Rakhman <lana.rakhman@gmail.com>
2016-05-15 11:51:48 -0500

    this commit should be removed along w/ the file changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook, :google, :twitter]
 
-	has_many :vacations, foreign_key: :tourist_id
+	has_many :vacations, foreign_key: :tourist_id, :tourist_visa, :tourism
   has_many :locations, through: :vacations
 
   has_many :user_meetups, foreign_key: :attendee_id
--- app/models/user_meetup.rb
+++ app/models/user_meetup.rb
@@ -1,4 +1,5 @@
 class UserMeetup < ActiveRecord::Base
 	belongs_to :meetup
   belongs_to :attendee, class_name: "User"
+#hey some more stuff here
 end
